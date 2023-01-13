837216e9f2b90e120958180bbacad278071a3c30
caoxm <386707656@qq.com>
2023-01-13 13:59:25 +0800

    change 'walletConnect to connect'&fix scroll things.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="isolate bg-white h-full overflow-y-auto">
+      <div className="isolate bg-white h-full overflow-y-auto flex items-center justify-center">
         <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
           <svg
             className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
@@ -141,30 +141,33 @@ export default function Home() {
             </defs>
           </svg>
         </div>
-        <div className="px-6 pt-6 lg:px-8">
-          <div>
-            <nav
-              className="flex h-9 items-center justify-between"
-              aria-label="Global"
-            >
-              <div className="flex min-w-0 flex-1 justify-end">
-                {currentAddress ? (
-                  `${currentNetwork}:${currentAddress}`
-                ) : (
-                  <button
-                    onClick={connectWallet}
-                    className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                  >
-                    Wallet connect
-                  </button>
-                )}
-              </div>
-            </nav>
+
+        <div className="absolute top-0 right-0">
+          <div className="px-6 pt-6 lg:px-8">
+            <div>
+              <nav
+                className="flex h-9 items-center justify-between"
+                aria-label="Global"
+              >
+                <div className="flex min-w-0 flex-1 justify-end">
+                  {currentAddress ? (
+                    `${currentNetwork}:${currentAddress}`
+                  ) : (
+                    <button
+                      onClick={connectWallet}
+                      className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                    >
+                      Connect
+                    </button>
+                  )}
+                </div>
+              </nav>
+            </div>
           </div>
         </div>
         <main>
           <div className="relative px-6 lg:px-8">
-            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+            <div className="mx-auto max-w-3xl pt-3 pb-5 sm:pt-10 sm:pb-10">
               <div>
                 <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                   <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -235,33 +238,6 @@ export default function Home() {
                       </p>
                     )}
                   </div>
-                </div>
-                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                  <svg
-                    className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                    viewBox="0 0 1155 678"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                      fillOpacity=".3"
-                      d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                    />
-                    <defs>
-                      <linearGradient
-                        id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                        x1="1155.49"
-                        x2="-78.208"
-                        y1=".177"
-                        y2="474.645"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stopColor="#9089FC" />
-                        <stop offset={1} stopColor="#FF80B5" />
-                      </linearGradient>
-                    </defs>
-                  </svg>
                 </div>
               </div>
             </div>
